4f2cec007413ba2646bbfeff2b1e9c63b204b066
Douglas <toddynotes@gmail.com>
2023-04-14 19:46:23 -0300

    refactor: added expandeds to control the widgets on screen

--- lib/pages/login_page_widget.dart
+++ lib/pages/login_page_widget.dart
@@ -18,91 +18,104 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const LogoWidget(),
-        const TextTitle(
-            text: 'Get your Money Under Control', textAlign: TextAlign.center),
-        const SizedBox(
-          height: 30,
-        ),
-        const TextSubtitle(
-          text: 'Manage your expenses.',
-          textAlign: TextAlign.center,
-        ),
-        const TextSubtitle(
-          text: 'Seamlessly.',
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(
-          height: 200,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
-          child: CustomElevatedButton(
-            onClick: () => print('clicked email'),
-            style: Style(
-              backgroundColor: const Color.fromRGBO(94, 92, 229, 1),
-              borderRadius: BorderRadius.circular(5),
-              width: screenWidth,
-              padding:
-                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-            ),
-            content: Content(
-              text: const Text(
-                'Sign Up with Email ID',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Expanded(flex: 4, child: LogoWidget()),
+          const Expanded(
+            flex: 2,
+            child: TextTitle(
+                text: 'Get your Money Under Control',
+                textAlign: TextAlign.center),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: const [
+                TextSubtitle(
+                  text: 'Manage your expenses.',
+                  textAlign: TextAlign.center,
+                ),
+                TextSubtitle(
+                  text: 'Seamlessly.',
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
           ),
-        ),
-        const SizedBox(
-          height: 14,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: CustomElevatedButton(
-            onClick: () => print('clicked google'),
-            style: Style(
-              backgroundColor: Colors.white,
-              borderRadius: BorderRadius.circular(5),
-              width: screenWidth,
-              padding:
-                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-            ),
-            content: Content(
-              leading: const Image(
-                image: AssetImage('assets/icons/google-color-icon.png'),
-                width: 20,
-                height: 20,
-              ),
-              text: const Text(
-                'Sign Up with Google',
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
-              ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: CustomElevatedButton(
+                    onClick: () => print('clicked email'),
+                    style: Style(
+                      backgroundColor: const Color.fromRGBO(94, 92, 229, 1),
+                      borderRadius: BorderRadius.circular(5),
+                      width: screenWidth,
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                    ),
+                    content: Content(
+                      text: const Text(
+                        'Sign Up with Email ID',
+                        style:
+                        TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: CustomElevatedButton(
+                    onClick: () => print('clicked google'),
+                    style: Style(
+                      backgroundColor: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      width: screenWidth,
+                      padding:
+                      const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                    ),
+                    content: Content(
+                      leading: const Image(
+                        image: AssetImage('assets/icons/google-color-icon.png'),
+                        width: 20,
+                        height: 20,
+                      ),
+                      text: const Text(
+                        'Sign Up with Google',
+                        style:
+                        TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextBody(text: 'Already have an account?'),
-            const SizedBox(
-              width: 2,
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const TextBody(text: 'Already have an account?'),
+                const SizedBox(
+                  width: 2,
+                ),
+                TextLink(
+                  text: 'Sign In',
+                  onClick: () => print('Sign In clicked'),
+                ),
+              ],
             ),
-            TextLink(
-              text: 'Sign In',
-              onClick: () => print('Sign In clicked'),
-            ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
